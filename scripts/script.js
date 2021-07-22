ec41be8f3abf3a7ce0efbae2686225fb8ef7953b
LukSiman <simanavicius.lukas@gmail.com>
2021-07-21 18:44:13 -0700

    adding painting of squares

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,14 @@
-const container = document.querySelector("container");
+const container = document.querySelector('#container');
 
-for(let i = 1; i <= 16; i++){
-    let div = document.createElement("div");
+for(let i = 1; i <= 16*16; i++){
+    let div = document.createElement('div');
+    div.classList.add('squares');
     container.appendChild(div);
 }
+
+const squares = document.querySelectorAll('.squares');
+
+squares.forEach(div => {
+    div.addEventListener('mouseenter', () => {
+        div.style.backgroundColor = 'black'});
+});
