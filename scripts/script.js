90afe3ed40ae8670a79736ebf5ebf6a920e4b7c1
LukSiman <simanavicius.lukas@gmail.com>
2021-07-25 10:46:46 -0700

    Adding grid resizing funtionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,55 @@
 const container = document.querySelector('#container');
+let squares = document.querySelectorAll('.squares');
 
-for(let i = 1; i <= 16*16; i++){
-    let div = document.createElement('div');
-    div.classList.add('squares');
-    container.appendChild(div);
+//let firstTime = true;
+
+//if(firstTime){
+    makeGrid(16); // initial grid
+  //  firstTime = false;
+//}
+
+function makeGrid(size){
+    container.innerHTML = '';
+
+    container.style.gridTemplateRows = `repeat(${size}, 30px)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 30px)`;
+
+    for (let i = 1; i <= size * size; i++) {
+        let div = document.createElement('div');
+        div.classList.add('squares');
+        container.appendChild(div);
+    }
+
+    squares = document.querySelectorAll('.squares');
+
+    squares.forEach(div => {
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = 'black'
+        });
+    });
 }
 
-const squares = document.querySelectorAll('.squares');
 
-squares.forEach(div => {
-    div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = 'black'});
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    size = 0;
+
+    while(size <= 0 || size > 100){
+
+    size = prompt('How many squares per side would you like?');
+
+        if(size > 100){
+            alert('Please enter 100 or less!');
+        }
+    }
+
+    makeGrid(size);
+
+    container.style.gridTemplateRows = `repeat(${size}, 30px)`;
+    container.style.gridTemplateColumns = `repeat(${size}, 30px)`;
+
+    squares.forEach(div => {
+        div.style.backgroundColor = 'white'
+    });
 });
